9f6358cc5f10b852cf71824361b1714e4959abfc
kneskoromny <kneskoromny@gmail.com>
2021-06-05 15:47:29 +0300

    Initial Commit

---
+++ ViewAndSlidersSwiftUIHW/ViewAndSlidersSwiftUIHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAndSlidersSwiftUIHWApp.swift
+//  ViewAndSlidersSwiftUIHW
+//
+//  Created by Кирилл Нескоромный on 05.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewAndSlidersSwiftUIHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
